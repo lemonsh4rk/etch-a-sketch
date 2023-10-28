bb0b2d99171c1bb0d659169676d7eb896518a8cc
lemonshark <83254974+lemonsh4rk@users.noreply.github.com>
2023-10-28 13:40:19 -0300

    feat: more responsive page for mobile

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,31 @@
 let divContainer = document.querySelector("#boxcontainer");
-let squareDiv;
+let sliderElement = document.querySelector("#slider");
+
+function getSquares() {
+
+  function getSliderValue() {
+    let sliderValue = sliderElement.value;
+    return sliderValue * sliderValue;
+  }
+
+  let manySquares = getSliderValue();
+
+  for (let i = 0; i <  manySquares; i++) {
+    let squareDiv = document.createElement("div");
 
 
-for (let i = 0; i < 256 ; i++) {
-  let squareDiv = document.createElement("div");
-  squareDiv.classList.add("square");
-  divContainer.appendChild(squareDiv);
+    squareDiv.classList.add("square");
+    divContainer.appendChild(squareDiv);
 
-  squareDiv.addEventListener("mouseover", () => {
-    squareDiv.classList.add("hoverbox");
-  })
+    squareDiv.addEventListener("mouseover", () => {
+      squareDiv.classList.add("hoverbox");
+    })
+  }
+}
+
+sliderElement.addEventListener("input", getSquares())
+
+
+function removeSquares() {
+  document.querySelectorAll("square").remove();
 }
