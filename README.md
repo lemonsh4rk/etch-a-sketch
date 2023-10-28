1648b536be38146f9fb0040dd2ab5de0a7580336
lemonshark <83254974+lemonsh4rk@users.noreply.github.com>
2023-10-27 21:48:53 -0300

    feat: add mouseover effect

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ TOP - Etch-a-sketch project!
 #### References
 
 - Project instructions: https://www.theodinproject.com/lessons/foundations-etch-a-sketch
+- Permanent hover effect - https://stackoverflow.com/questions/44512514/how-to-make-hover-effect-stays-even-after-unhover
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,14 @@
 let divContainer = document.querySelector("#boxcontainer");
-
+let squareDiv;
 
 
 for (let i = 0; i < 256 ; i++) {
   let squareDiv = document.createElement("div");
-  squareDiv.classList.add("square")
+  squareDiv.classList.add("square");
   divContainer.appendChild(squareDiv);
+
+  squareDiv.addEventListener("mouseover", () => {
+    squareDiv.classList.add("hoverbox");
+  })
 }
+
